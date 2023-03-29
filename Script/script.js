82b4d3b539a4539d6f9076c5ed5808a736111cec
Jorge Cano <coronero@gmail.com>
2023-03-28 19:43:42 -0700

    TaskManager-Assignment2-ver1

--- Script/script.js
+++ Script/script.js
@@ -1,3 +1,5 @@
+var isImportant = false;
+
 //function to toggle side panel
 function togglePanel(){
     console.log("button clicked");
@@ -9,16 +11,56 @@ function togglePanel(){
 //function to save task
 function saveTask(){
     console.log("task saved");
+
+    const title = $("#txtTask").val();
+    const descript = $("#txtDescript").val();
+    const due = $("#selDue").val();
+    const duration = $("#txtDuration").val();
+    const status = $("#selStatus").val();
+    const color = $("#txtColor").val();
+
+    let task = new Task(title, isImportant, descript, due, duration, status, color);
+
+    console.log(task);
+    displayTask(task);
+    
 }
+
+//function to display inputed tasks
+function displayTask(task){
+    let syntax = `
+        <div class="task" style="border: 2px solid ${task.color};">
+            <div class="info">
+            <h5>${task.title}</h5>
+            <p>${task.descript}</p>
+            </div>
+
+            <label>${task.status}</label>
+
+            <div class="dates">
+            <label>${task.due}</label>
+            <label>${task.duration} days</label>
+            </div>
+        </div>`
+
+    $("#pendingTasks").append(syntax);
+
+}
+
 
 //function to toggle bookmark icon
 function toggleImportant(){
     const nonImpClasses = "fa-regular fa-bookmark notImp";
     const impClasses = "fa-solid fa-bookmark imp";
-    console.log("Important icon clicked.")
 
-    $("#iImportant").removeClass(nonImpClasses);
-    $("#iImportant").addClass(impClasses);
+    if(isImportant){
+        $("#iImportant").removeClass(impClasses).addClass(nonImpClasses);
+        isImportant = false;
+    }
+    else{
+        $("#iImportant").removeClass(nonImpClasses).addClass(impClasses);
+        isImportant = true;
+    }
 }
 
 
---
+++ Script/task.js
@@ -0,0 +1,16 @@
+class Task{
+    constructor(title, important, descript, due, duration, status, color){
+        this.title = title;
+        this.important = important;
+        this.descript = descript;
+        this.due = due;
+        this.duration = duration;
+        this.status = status;
+        this.color = color;
+
+        this.name = "Jorge";
+
+
+
+    }
+}
